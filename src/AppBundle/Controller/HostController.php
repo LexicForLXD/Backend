740955caec1523ed830cc3b528c2a7da8ea12e58
Unknown <ich@leonlenzen.de>
2017-11-12 13:51:44 +0100

    Updated: example for index and store in HostController

--- src/AppBundle/Controller/HostController.php
+++ src/AppBundle/Controller/HostController.php
@@ -11,19 +11,53 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Host;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class HostController extends Controller
 {
     /**
      * @Route("/hosts", name="hosts_index")
+     * @Method({"GET"})
+     * @return Response
      */
-    public function index()
+    public function indexAction()
     {
         $hosts = $this->getDoctrine()->getRepository(Host::class)->findAll();
 
-        return new JsonResponse($hosts);
+        $serializer = $this->get('jms_serializer');
+        $response = $serializer->serialize($hosts, 'json');
+        return new Response($response);
+    }
+
+
+    /**
+     * @Route("/hosts", name="hosts_store")
+     * @Method({"POST"})
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function storeAction(Request $request, EntityManagerInterface $em)
+    {
+
+        $host = new Host();
+        $host->setIpv4($request->request->get('ipv4'));
+        $host->setIpv6($request->request->get('ipv6'));
+        $host->setDomainName($request->request->get('domain_name'));
+        $host->setMac($request->request->get('mac'));
+        $host->setName($request->request->get('name'));
+        $host->setSettings($request->request->get('settings'));
+        $em->persist($host);
+        $em->flush();
+
+        $serializer = $this->get('jms_serializer');
+        $response = $serializer->serialize($host, 'json');
+        return new Response($response);
     }
 
 }
