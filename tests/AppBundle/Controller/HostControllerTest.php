c0f4f5cc5a89601fd833c3b99d2413aefa5ca45b
Jan Ritter <jan-ritter@outlook.com>
2017-11-15 21:55:32 +0100

    updated: added authentication

--- tests/AppBundle/Controller/HostControllerTest.php
+++ tests/AppBundle/Controller/HostControllerTest.php
@@ -7,22 +7,47 @@ use AppBundle\Entity\Host;
 
 class HostControllerTest extends WebTestCase
 {
+
+    protected $token;
+
+    protected function setUp()
+    {
+        $client = static::createClient();
+        
+                $client->request(
+                    'POST',
+                    '/oauth/v2/token',
+                    array(),
+                    array(),
+                    array('CONTENT_TYPE' => 'application/json'),
+                    '{
+                        "grant_type": "password",
+                        "client_id": "1_3bcbxd9e24g0gk4swg0kwgcwg4o8k8g4g888kwc44gcc0gwwk4",
+                        "client_secret": "4ok2x70rlfokc8g0wws8c8kwcokw80k44sg48goc0ok4w0so0k",
+                        "username": "mmustermann",
+                        "password": "password"
+                    }'
+                );
+
+                $result =  json_decode($client->getResponse()->getContent());
+                $this->token = 'Bearer '.$result->access_token;
+    }
+
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hosts');
+        $client->request(
+            'GET',
+            '/hosts',
+            array(),
+            array(),
+            array(
+                'CONTENT_TYPE' => 'application/json',
+                'HTTP_Authorization' => $this->token
+            )
+        );
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-    }
-
-    public function testShow()
-    {
-        $client = static::createClient();
-
-        $host = new Host();
-
-        $crawler = $client->request('GET', '/hosts/1');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
@@ -35,7 +60,8 @@ class HostControllerTest extends WebTestCase
             '/hosts',
             [],
             [],
-            [],
+            [ 'CONTENT_TYPE' => 'application/json',
+            'HTTP_Authorization' => $this->token ],
             '{
                 "ipv4": "192.168.1.2",
                 "ipv6": "asjhdfskjfh",
@@ -47,6 +73,25 @@ class HostControllerTest extends WebTestCase
         );
 
         $this->assertEquals(201, $client->getResponse()->getStatusCode());
+    }
 
+    public function testShow()
+    {
+        $client = static::createClient();
+
+        $host = new Host();
+
+        $client->request(
+            'GET',
+            '/hosts/1',
+            array(),
+            array(),
+            array(
+                'CONTENT_TYPE' => 'application/json',
+                'HTTP_Authorization' => $this->token
+            )
+        );
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 }
