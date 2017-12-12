a984ca7281a375ae19769942aa2d22a5b9bcbfeb
Jan Ritter <jan-ritter@outlook.com>
2017-12-12 14:23:40 +0100

    fix: added Swagger Information to the default Controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,11 +5,16 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Swagger\Annotations as SWG;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @SWG\Info(
+     *     title="Lexic API",
+     *     version=1.0
+     * )
      */
     public function indexAction(Request $request)
     {
