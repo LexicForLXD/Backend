6924ef58a5599132b962d5b1bd595584b9dce38e
Jan Ritter <jan-ritter@outlook.com>
2017-12-26 10:40:43 +0100

    feat: added entity for Images

---
+++ src/AppBundle/Entity/Image.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace AppBundle\Entity;
+
+
+/**
+ * Class Container
+ * @package AppBundle\Entity
+ * @ORM\Table(name="images")
+ */
+class Image
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $fingerprint;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $alias;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $architecture;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $size;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFingerprint()
+    {
+        return $this->fingerprint;
+    }
+
+    /**
+     * @param mixed $fingerprint
+     */
+    public function setFingerprint($fingerprint)
+    {
+        $this->fingerprint = $fingerprint;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAlias()
+    {
+        return $this->alias;
+    }
+
+    /**
+     * @param mixed $alias
+     */
+    public function setAlias($alias)
+    {
+        $this->alias = $alias;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getArchitecture()
+    {
+        return $this->architecture;
+    }
+
+    /**
+     * @param mixed $architecture
+     */
+    public function setArchitecture($architecture)
+    {
+        $this->architecture = $architecture;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    /**
+     * @param mixed $size
+     */
+    public function setSize($size)
+    {
+        $this->size = $size;
+    }
+
+
+}
