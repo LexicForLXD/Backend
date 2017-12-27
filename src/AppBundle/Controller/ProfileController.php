9d4e12ab4ed8377b312c0223f6b5a82780d69dd2
Jan Ritter <jan-ritter@outlook.com>
2017-12-28 00:10:44 +0100

    feat: added function stubs for lxc-profile functions

--- src/AppBundle/Controller/ProfileController.php
+++ src/AppBundle/Controller/ProfileController.php
@@ -5,13 +5,14 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Profile;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProfileController extends Controller
 {
     /**
      * Get all LXC-Profiles
      *
-     * @Route("/images", name="images_all", methods={"GET"})
+     * @Route("/profiles", name="profiles_all", methods={"GET"})
      */
     public function getAllProfiles(){
         $profiles = $this->getDoctrine()->getRepository(Profile::class)->findAll();
@@ -25,6 +26,42 @@ class ProfileController extends Controller
         $serializer = $this->get('jms_serializer');
         $response = $serializer->serialize($profiles, 'json');
         return new Response($response);
+    }
+
+    /**
+     * Get a single LXC-Profile by its id
+     *
+     * @Route("/profiles/{profileId}", name="profile_single", methods={"GET"})
+     */
+    public function getSingleProfile($profileId){
+
+    }
+
+    /**
+     * Create a LXC-Profile
+     *
+     * @Route("/profiles", name="create_profile", methods={"POST"})
+     */
+    public function createProfile(){
+
+    }
+
+    /**
+     * Edit a existing LXC-Profile
+     *
+     * @Route("/profiles/{profileId}", name="edit_profile", methods={"PUT"})
+     */
+    public function editProfile($profileId){
+
+    }
+
+    /**
+     * Delete a existing LXC-Profile
+     *
+     * @Route("/profiles/{profileId}", name="delete_profile", methods={"DELETE"})
+     */
+    public function deleteProfile($profileId){
+
     }
 
 }
