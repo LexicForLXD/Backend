8af620dde7aa8390905574ad2e4f4057a942b5a5
Jan Ritter <jan-ritter@outlook.com>
2018-01-04 16:46:11 +0100

    feat: added function to create a Remote-Image

--- src/AppBundle/Controller/ImageController.php
+++ src/AppBundle/Controller/ImageController.php
@@ -2,11 +2,15 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Host;
 use AppBundle\Entity\Image;
+use AppBundle\Entity\ImageAlias;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Swagger\Annotations as OAS;
+use Symfony\Component\VarDumper\VarDumper;
 
 class ImageController extends Controller
 {
@@ -90,18 +94,67 @@ class ImageController extends Controller
     }
 
     /**
-     * Create a new Image on a specific Host
+     * Create a new Remote-Image on a specific Host
      *
      *
-     * @Route("/hosts/{hostId}/images", name="create_image_on_host", methods={"POST"})
+     * @Route("/hosts/{hostId}/images/remote", name="create_remote_image_on_host", methods={"POST"})
      *
      * @OAS\Post(path="/hosts/{hostId}/images",
      *     tags={"images"},
      *     description="TO BE DEFINED"
      * )
      */
-    public function createNewImageOnHost($hostId){
+    public function createNewRemoteSourceImageOnHost($hostId, Request $request){
+        $host = $this->getDoctrine()->getRepository(Host::class)->find($hostId);
 
+        if (!$host) {
+            throw $this->createNotFoundException(
+                'No Host for '.$hostId.' found'
+            );
+        }
+
+        $image = new Image();
+        $image->setHost($host);
+
+        if($request->request->get('filename')) {
+            $image->setFilename($request->request->get('filename'));
+        }
+        if($request->request->get('public')) {
+            $image->setPublic($request->request->get('public'));
+        }
+        if($request->request->get('properties')) {
+            $image->setProperties($request->request->get('properties'));
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        //Create aliases
+        if($request->request->get('aliases')) {
+            VarDumper::dump($request->request->get('aliases'));
+            $aliasArray = $request->request->get('aliases');
+
+            for($i=0; $i<sizeof($aliasArray); $i++){
+                $alias = new ImageAlias();
+                //TODO Validate if all necessary parameters were provided
+                $alias->setName($aliasArray[$i]['name']);
+                $alias->setDescription($aliasArray[$i]['description']);
+                $em->persist($alias);
+                $image->addAlias($alias);
+            }
+        }
+
+        //TODO add LXD API call based on source data
+        //Will be set with the value provided by the lxd api response
+
+        $image->setFingerprint("JSKAJLSJASJ21344A3A3S1A2S34S5A6S");
+        $image->setArchitecture("amd64");
+        $image->setSize(12346);
+
+        $em->persist($image);
+        $em->flush();
+
+        $serializer = $this->get('jms_serializer');
+        $response = $serializer->serialize($image, 'json');
+        return new Response($response);
     }
 
     /**
