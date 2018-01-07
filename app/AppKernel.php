021a263c7a2bff03a44206c7293a523fb56c8f04
Jan Ritter <jan-ritter@outlook.com>
2018-01-07 11:17:18 +0100

    feat: enabled redis bundle in AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Nelmio\CorsBundle\NelmioCorsBundle(),
             new SymfonyBundles\EventQueueBundle\SymfonyBundlesEventQueueBundle(),
+            new SymfonyBundles\RedisBundle\SymfonyBundlesRedisBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
