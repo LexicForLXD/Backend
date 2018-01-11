a6693d887c18458067b025024d3c80c3cdeffb4a
Jan Ritter <jan-ritter@outlook.com>
2018-01-11 21:00:12 +0100

    feat: added ImageAliasApi to delete an alias for an image by it's name

---
+++ src/AppBundle/Service/LxdApi/ImageAliasApi.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Service\LxdApi;
+
+
+use AppBundle\Entity\Host;
+use AppBundle\Service\LxdApi\Util\HttpHelper;
+use Httpful\Request;
+
+class ImageAliasApi extends HttpHelper
+{
+
+    protected function getEndpoint($urlParam = NULL)
+    {
+        return 'images/aliases';
+    }
+
+    /**
+     * ImageApi constructor.
+     * @param $cert_location
+     * @param $cert_key_location
+     * @param $cert_passphrase
+     * @throws \AppBundle\Exception\WrongInputException
+     */
+    public function __construct($cert_location, $cert_key_location, $cert_passphrase)
+    {
+        parent::__construct($cert_location, $cert_key_location, $cert_passphrase);
+        $this->init();
+    }
+
+    /**
+     * @param Host $host
+     * @param String $name
+     * @return \Httpful\Response
+     * @throws \Httpful\Exception\ConnectionErrorException
+     */
+    public function removeAliasByName(Host $host, String $name){
+        $uri = $this->buildUri($host, $this->getEndpoint().'/'.$name);
+        return Request::delete($uri)
+            -> send();
+    }
+
+}
