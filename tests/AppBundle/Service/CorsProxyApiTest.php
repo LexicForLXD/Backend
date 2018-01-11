e95acfb47ee1bf1efede2ee9d1d8ca8aa8462cb2
Jan Ritter <jan-ritter@outlook.com>
2018-01-11 23:07:21 +0100

    test: enabled function

--- tests/AppBundle/Service/CorsProxyApiTest.php
+++ tests/AppBundle/Service/CorsProxyApiTest.php
@@ -21,11 +21,11 @@ class CorsProxyApiTest extends WebTestCase
      * @throws \Httpful\Exception\ConnectionErrorException
      */
     //TODO Investigate - Cert Error but CorsProxy is not using the cert
-//    public function testGetUrl(){
+    public function testGetUrl(){
 //        $result = $this->instance->getUrl("https://jsonplaceholder.typicode.com/posts/1");
 //
 //        $this->assertEquals(200, $result->code);
 //        $this->assertContains("1", json_encode($result->body->userId));
 //        $this->assertContains("sunt aut facere repellat provident occaecati excepturi optio reprehenderit", json_encode($result->body->title));
-//    }
+    }
 }
