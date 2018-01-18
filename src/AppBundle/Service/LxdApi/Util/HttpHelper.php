ec14bc7f93679f1090c0501cb52a96e0d5db4631
Jan Ritter <jan-ritter@outlook.com>
2018-01-18 21:50:04 +0100

    fix: removed double use of Host

--- src/AppBundle/Service/LxdApi/Util/HttpHelper.php
+++ src/AppBundle/Service/LxdApi/Util/HttpHelper.php
@@ -6,7 +6,6 @@ namespace AppBundle\Service\LxdApi\Util;
 use AppBundle\Entity\Host;
 use AppBundle\Exception\WrongInputException;
 use Httpful\Request;
-use AppBundle\Entity\Host;
 
 class HttpHelper
 {
