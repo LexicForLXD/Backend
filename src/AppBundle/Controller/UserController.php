e54c23527a1b005389d533e3392bc50f895ff34a
Leon Lenzen <ich@leonlenzen.de>
2018-01-23 13:41:47 +0100

    feat: show all Users

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -12,7 +12,40 @@ use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+use Doctrine\ORM\EntityManagerInterface;
+
 class UserController
 {
 
+    /**
+     * Gibt eine Liste all
+     *
+     * @Route("/users", name="users_all", methods={"GET"})
+     *
+     * @OAS\Get(path="/users",
+     *     tags={"users"},
+     *      @OAS\Response(
+     *          response=200,
+     *          description="List of all users",
+     *          @OAS\JsonContent(ref="#/components/schemas/user"),
+     *          @OAS\Schema(
+     *              type="array"
+     *          ),
+     *      ),
+     *      @OAS\Response(
+     *          response=404,
+     *          description="No users found",
+     *      ),
+     * )
+     *
+     * @return Response
+     */
+    public function indexAction()
+    {
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        $serializer = $this->get('jms_serializer');
+        $response = $serializer->serialize($users, 'json');
+        return new Response($response);
+    }
 }
