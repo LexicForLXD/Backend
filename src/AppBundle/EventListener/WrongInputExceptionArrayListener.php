11f3a5432e5944135fe0605254f80f3137aec29b
Jan Ritter <jan-ritter@outlook.com>
2018-02-06 20:20:11 +0100

    feat: added custom exception supporting array instead of string as message - relates to #49

---
+++ src/AppBundle/EventListener/WrongInputExceptionArrayListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+
+use AppBundle\Exception\WrongInputException;
+use AppBundle\Exception\Utils\UserInputException;
+use AppBundle\Exception\WrongInputExceptionArray;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+class WrongInputExceptionArrayListener
+{
+    public function onKernelException(GetResponseForExceptionEvent $event){
+        $exception = $event->getException();
+
+        if(!$exception instanceof WrongInputExceptionArray){
+            return;
+        }
+
+        $code = $exception instanceof UserInputException ? 400 : 500;
+
+        $responseData = [
+            'error' => [
+                'code' => $code,
+                'message' => $exception->getMessage()
+            ]
+        ];
+
+        $event->setResponse(new JsonResponse($responseData, $code));
+    }
+}
