fe2657aa12799c9864a01c294ebc0a7ce9a42cc3
Leon Lenzen <ich@leonlenzen.de>
2018-02-27 11:25:02 +0100

    test: create user command

--- tests/AppBundle/Command/CreateUserCommandTest.php
+++ tests/AppBundle/Command/CreateUserCommandTest.php
@@ -9,12 +9,29 @@ use Symfony\Component\Console\Tester\CommandTester;
 
 class CreateUserCommandTest extends KernelTestCase
 {
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    private $encoder;
+
+    public function setUp()
+    {
+
+        static::$kernel = static::createKernel();
+        static::$kernel->boot();
+        $this->em = static::$kernel->getContainer()->get('doctrine')->getManager();
+        $this->encoder = static::$kernel->getContainer()->get('security.password_encoder');
+    }
+
+
     public function testExecute()
     {
         $kernel = self::bootKernel();
         $application = new Application($kernel);
 
-        $application->add(new CreateUserCommand());
+        $application->add(new CreateUserCommand('app:create-user', $this->em, $this->encoder));
 
         $command = $application->find('app:create-user');
         $commandTester = new CommandTester($command);
