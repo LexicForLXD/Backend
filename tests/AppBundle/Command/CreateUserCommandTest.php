261271291d3c278936c952a5dc1c8f4b7f4e7c5b
Leon Lenzen <ich@leonlenzen.de>
2018-02-28 21:58:08 +0100

    test: test for CreateUserCommand with valid and invalid

--- tests/AppBundle/Command/CreateUserCommandTest.php
+++ tests/AppBundle/Command/CreateUserCommandTest.php
@@ -6,6 +6,7 @@ use AppBundle\Command\CreateUserCommand;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
+use AppBundle\Entity\User;
 
 class CreateUserCommandTest extends KernelTestCase
 {
@@ -15,42 +16,125 @@ class CreateUserCommandTest extends KernelTestCase
     private $em;
 
     private $encoder;
+    private $command;
+
 
     public function setUp()
     {
 
-        static::$kernel = static::createKernel();
-        static::$kernel->boot();
-        $this->em = static::$kernel->getContainer()->get('doctrine')->getManager();
-        $this->encoder = static::$kernel->getContainer()->get('security.password_encoder');
+        $kernel = static::createKernel();
+        $kernel->boot();
+
+
+        $application = new Application($kernel);
+
+
+        $this->em = $kernel->getContainer()->get('doctrine')->getManager();
+        $this->encoder = $kernel->getContainer()->get('security.password_encoder');
+
+        $application->add(new CreateUserCommand('app:create-user', $this->em, $this->encoder));
+        $this->command = $application->find('app:create-user');
     }
 
 
-    public function testExecute()
+    public function testCreationValid()
     {
-        $kernel = self::bootKernel();
-        $application = new Application($kernel);
 
-        $application->add(new CreateUserCommand('app:create-user', $this->em, $this->encoder));
+        $commandTester = new CommandTester($this->command);
 
-        $command = $application->find('app:create-user');
-        $commandTester = new CommandTester($command);
-        $commandTester->execute(array(
-            'command'  => $command->getName()
-        ));
         $commandTester->setInputs(array(
             'FirstNameCommand',
             'LastNameCommand',
             'email@command.de',
+            'commandUsername',
             'password',
             'password'
         ));
 
+        $commandTester->execute(array(
+            'command'  => $this->command->getName()
+        ));
 
         // the output of the command in the console
         $output = $commandTester->getDisplay();
         $this->assertContains('User erfolgreich erzeugt', $output);
 
-        // ...
+        $userDB = $this->em->getRepository(User::class)->findOneBy(['username' => 'commandUsername']);
+
+        $this->em->remove($userDB);
+        $this->em->flush();
+
+    }
+
+    public function testCreationInvalidPassword()
+    {
+
+        $commandTester = new CommandTester($this->command);
+
+        $commandTester->setInputs(array(
+            'FirstNameCommand',
+            'LastNameCommand',
+            'email@command.de',
+            'commandUsername',
+            'password',
+            'password1',
+            'password',
+            'password1'
+        ));
+
+        $commandTester->execute(array(
+            'command'  => $this->command->getName()
+        ));
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+        $this->assertContains('Die Passwörter stimmen nicht überein.', $output);
+
+        $userDB = $this->em->getRepository(User::class)->findOneBy(['username' => 'commandUsername']);
+
+    }
+
+    public function testCreationAlreadyExisting()
+    {
+        $user = new User();
+        $user->setFirstName('FirstNameCommand');
+        $user->setLastName('LastNameCommand');
+        $user->setEmail('email@command.de');
+        $user->setUsername('commandUsername');
+        $user->setPassword('password');
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+
+        $commandTester = new CommandTester($this->command);
+
+        $commandTester->setInputs(array(
+            'FirstNameCommand',
+            'LastNameCommand',
+            'email@command.de',
+            'email1@command.de',
+            'commandUsername',
+            'command1Username',
+            'password',
+            'password'
+        ));
+
+        $commandTester->execute(array(
+            'command'  => $this->command->getName()
+        ));
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+
+        $this->assertContains('Der Benutzername ist nicht eindeutig.', $output);
+        $this->assertContains('Die email ist nicht eindeutig.', $output);
+
+
+        $userDB = $this->em->getRepository(User::class)->findOneBy(['username' => 'command1Username']);
+
+        $this->em->remove($userDB);
+        $this->em->remove($user);
+        $this->em->flush();
     }
 }
