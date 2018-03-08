4ca8e905627fc19545fd2ecce9ae0863809242fa
Jan Ritter <jan-ritter@outlook.com>
2018-03-08 11:33:41 +0100

    feat: added endpoint to get single Backup object by it's id

--- src/AppBundle/Controller/BackupController.php
+++ src/AppBundle/Controller/BackupController.php
@@ -49,4 +49,42 @@ class BackupController extends Controller
         $response = $serializer->serialize($backups, 'json');
         return new Response($response);
     }
+
+    /**
+     * Get single Backup by it's id
+     *
+     * @Route("/backups/{id}", name="backup_by_id", methods={"GET"})
+     *
+     * @return Response
+     * @throws ElementNotFoundException
+     *
+     * @OAS\Get(path="/backups/{id}",
+     *     tags={"backups"},
+     *      @OAS\Response(
+     *          response=200,
+     *          description="Single Backup with the provided id",
+     *          @OAS\JsonContent(ref="#/components/schemas/backup"),
+     *          @OAS\Schema(
+     *              type="object"
+     *          ),
+     *      ),
+     *      @OAS\Response(
+     *          response=404,
+     *          description="No Backup for the id found",
+     *      ),
+     * )
+     */
+    public function getBackupById($id){
+        $backup = $this->getDoctrine()->getRepository(Backup::class)->find($id);
+
+        if (!$backup) {
+            throw new ElementNotFoundException(
+                'No Backup for id '.$id .' found'
+            );
+        }
+
+        $serializer = $this->get('jms_serializer');
+        $response = $serializer->serialize($backup, 'json');
+        return new Response($response);
+    }
 }
