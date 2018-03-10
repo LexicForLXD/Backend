2d89b6a7d57d0b72308a5cbb0427153461052e9c
Jan Ritter <jan-ritter@outlook.com>
2018-03-10 23:07:22 +0100

    test: added tests for getter and setter methods of Backup Entity

---
+++ tests/AppBundle/Entity/BackupEntityTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\AppBundle\Entity;
+
+use AppBundle\Entity\Backup;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Config\Definition\Exception\Exception;
+
+class BackupEntityTest extends WebTestCase
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        static::$kernel = static::createKernel();
+        static::$kernel->boot();
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager()
+        ;
+
+    }
+
+    /**
+     * Check all getter for minimal allowed attributes in Entity - only notNull attributes set
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Exception
+     */
+    public function testGetterMinimalAttributesSet()
+    {
+        $backup = new Backup();
+
+        $backup->setTimestamp();
+        $backup->setFilePath("/backups/46876a46467645as6d3763.tar.gz");
+
+        $this->em->persist($backup);
+        $this->em->flush();
+
+        $backupFromDB = $this->em->getRepository(Backup::class)->find($backup->getId());
+
+        $this->assertEquals($backup->getTimestamp(), $backupFromDB->getTimestamp());
+        $this->assertEquals($backup->getFilePath(), $backupFromDB->getFilePath());
+        $this->assertEquals($backup->getId(), $backupFromDB->getId());
+
+        $this->em->remove($backupFromDB);
+        $this->em->flush();
+    }
+
+    /**
+     * Check if all setters allow wrong values to allow validation
+     * @throws \Exception
+     */
+    public function testWrongAttributes()
+    {
+        $exception = false;
+        try {
+            $backup = new Backup();
+            $backup->setFilePath(0);
+        }catch (Exception $e){
+            $exception = true;
+        }
+
+        $this->assertTrue(!$exception); // Should be false = no exception triggered
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+        $this->em->close();
+    }
+}
