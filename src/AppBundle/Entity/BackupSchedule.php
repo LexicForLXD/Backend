fcada9a95606c152a07da1dccc9f7dab1c787b25
Leon Lenzen <ich@leonlenzen.de>
2018-03-13 11:39:04 +0100

    refactor: Backupschedule uses one file per schedule

--- src/AppBundle/Entity/BackupSchedule.php
+++ src/AppBundle/Entity/BackupSchedule.php
@@ -309,38 +309,49 @@ class BackupSchedule
     /**
      * Returns the Commands which will be written in a shell script.
      *
-     * @return Array
+     * @return string
      */
     public function getShellCommands()
     {
-        $commandTexts = array();
+        $commandTexts = '#!/bin/sh \n \n';
+
 
         foreach ($this->containers as $container) {
-            $commandTexts[] = '
-                #!/bin/sh \n
-                \n
+            $commandTexts = $commandTexts . '
                 # Backup for Container ' . $container->getName() . ' to ' . $this->destination->getName() . '\n
                 \n
                 # Just generating a random number \n
-                r=$(($(od -An -N1 -i /dev/random))) \n \n
+                r=$(($(od -An -N1 -i /dev/random))) \n
+                \n
                 # Generating a snapshot of the container to build the image from \n
-                lxc snapshot ' . $container->getName() . '/"$r" \n \n
+                lxc snapshot ' . $container->getName() . '/"$r" \n
+                \n
                 # Build the image to be exported \n
                 f=$(lxc publish ' . $container->getName() . '/"$r") \n
-                fingerprint=${f##*: } \n \n
+                fingerprint=${f##*: } \n
+                \n
+                # Make dir for backups in /tmp \n
+                mkdir /tmp/' . $this->name . ' \n
+                \n
                 # Export the image \n
-                lxc image export "$fingerprint" /tmp/' . $container->getName() . ' \n \n
-                # Backup via duplicity \n
-                duplicity ' . $this->type . ' /tmp/' . $container->getName() . ' ' . $this->destination->getDestinationText() . $container->getName() . ' \n \n
+                lxc image export "$fingerprint" /tmp/' . $this->name . '/' . $container->getName() . ' \n
+                \n
+
                 # Delete the snapshot \n
-                lxc delete ' . $container->getName() . '/"$r" \n \n
+                lxc delete ' . $container->getName() . '/"$r" \n
+                \n
                 # Delete the image \n
-                lxc image delete "$fingerprint"\n
+                lxc image delete "$fingerprint"\n \n \n
             ';
         }
 
+        $commandTexts = $commandTexts .
+            '# Backup via duplicity \n
+            duplicity ' . $this->type . ' / tmp / ' . $this->name . ' ' . $this->destination->getDestinationText() . $this->name . ' \n\n';
+
         return $commandTexts;
     }
+
 
     /**
      * @return string
