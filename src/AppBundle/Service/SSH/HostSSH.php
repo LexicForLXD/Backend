14622ad445ce69f2b9f08fe15841ea86e6ebd4ca
Jan Ritter <jan-ritter@outlook.com>
2018-03-14 21:24:34 +0100

    refactor: changed restoreForTimestamp command to only restore the tarball for the required Container

--- src/AppBundle/Service/SSH/HostSSH.php
+++ src/AppBundle/Service/SSH/HostSSH.php
@@ -107,9 +107,10 @@ class HostSSH
      * @param \DateTime $timestamp
      * @param BackupDestination $backupDestination
      * @param string $destinationPath
+     * @param string $containerName
      * @param Host $host
      */
-    public function restoreBackupForTimestampInTmp(\DateTime $timestamp, BackupDestination $backupDestination, string $destinationPath, Host $host)
+    public function restoreBackupForTimestampInTmp(\DateTime $timestamp, BackupDestination $backupDestination, string $destinationPath, string $containerName, Host $host)
     {
         $hostname = $host->getIpv4() ? : $host->getIpv6() ? : $host->getDomainName() ? : 'localhost';
         $configuration = new Configuration($hostname);
@@ -121,9 +122,10 @@ class HostSSH
 
         $remoteBackupPath = $backupDestination->getDestinationText().$destinationPath;
 
-        $duplicityCommand = 'duplicity restore --no-encryption '.$remoteBackupPath.' --time '.date_format($timestamp, DATE_ISO8601).' /tmp/restore'.$destinationPath;
+        $duplicityCommand = 'duplicity restore --no-encryption '.$remoteBackupPath.' --time '.date_format($timestamp, DATE_ISO8601).' --file-to-restore '.$containerName.'.tar.gz /tmp/restore'.$destinationPath.'/'.$containerName.'.tar.gz';
 
         $exec->run('rm -rf /tmp/restore'.$destinationPath);
+        $exec->run('mkdir /tmp/restore'.$destinationPath);
         $result = $exec->run($duplicityCommand);
     }
 }
