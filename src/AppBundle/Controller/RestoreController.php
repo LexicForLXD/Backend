5924ad8b8d2eb5cd47ad457561f09f836f18c18e
Jan Ritter <jan-ritter@outlook.com>
2018-03-15 20:14:48 +0100

    feat: added container creation process to createContainerFromBackup function

--- src/AppBundle/Controller/RestoreController.php
+++ src/AppBundle/Controller/RestoreController.php
@@ -4,17 +4,20 @@ namespace AppBundle\Controller;
 
 
 use AppBundle\Entity\Backup;
+use AppBundle\Entity\Container;
 use AppBundle\Entity\Host;
 use AppBundle\Entity\Image;
 use AppBundle\Entity\ImageAlias;
 use AppBundle\Exception\ElementNotFoundException;
 use AppBundle\Exception\WrongInputException;
+use AppBundle\Service\LxdApi\ContainerApi;
 use AppBundle\Service\LxdApi\ImageApi;
 use AppBundle\Service\Restore\RestoreService;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class RestoreController extends Controller
 {
@@ -47,12 +50,18 @@ class RestoreController extends Controller
      * Restore Backups created via BackupSchedule
      *
      * @Route("/restores/backups/{backupID}", name="create_container_from_backup", methods={"POST"})
+     * @param $backupID
      * @param Request $request
-     * @throws WrongInputException
+     * @param EntityManagerInterface $entityManager
+     * @param RestoreService $restoreService
+     * @param ImageApi $imageApi
+     * @param ContainerApi $containerApi
+     * @return Response
      * @throws ElementNotFoundException
+     * @throws WrongInputException
      * @throws \Httpful\Exception\ConnectionErrorException
      */
-    public function createContainerFromBackup($backupID, Request $request, EntityManagerInterface $entityManager, RestoreService $restoreService, ImageApi $imageApi){
+    public function createContainerFromBackup($backupID, Request $request, EntityManagerInterface $entityManager, RestoreService $restoreService, ImageApi $imageApi, ContainerApi $containerApi){
         if(!$request->request->has('tarball') || !$request->request->has('containerName') || !$request->request->has('hostID')) {
             throw new WrongInputException('At least one of the required parameters in the body is missing');
         }
@@ -110,5 +119,34 @@ class RestoreController extends Controller
         }else{
             throw new WrongInputException("Couldn't import LXC Image from tarball - LXC Error : ".$result);
         }
+
+        //Create Container for the restored Image
+        $result = $restoreService->restoreContainerFromImage($host, $containerName);
+        if(strpos($result, 'error') !== false){
+
+            $result = $containerApi->show($host, $containerName);
+
+            $container = new Container();
+            $container->setName($containerName);
+            $container->setHost($host);
+            $container->setSettings($result->body->metadata);
+            $container->setState(strtolower($result->body->metadata->status));
+
+            $container->setImage($image);
+            $image->addContainer($container);
+
+            $entityManager->persist($container);
+            $entityManager->merge($image);
+
+            $entityManager->flush();
+
+        }else{
+            $error = substr(strpos($result, 'error'), $result);
+            throw new WrongInputException("Couldn't create Container from the imported Image - LXC Error : ".$error);
+        }
+
+        $serializer = $this->get('jms_serializer');
+        $response = $serializer->serialize($container, 'json');
+        return new Response($response, Response::HTTP_CREATED);
     }
 }
