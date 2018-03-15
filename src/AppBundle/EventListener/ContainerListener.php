88890776e7d5075a2dd3e4d3f2e7255089c7c534
Leon Lenzen <ich@leonlenzen.de>
2018-03-15 21:45:52 +0100

    feat: added listener for ContainerUpdateEvent

--- src/AppBundle/EventListener/ContainerListener.php
+++ src/AppBundle/EventListener/ContainerListener.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: lionf
@@ -17,6 +18,7 @@ use AppBundle\Service\LxdApi\ContainerStateApi;
 use AppBundle\Service\LxdApi\OperationApi;
 use AppBundle\Service\Profile\ProfileManagerApi;
 use Doctrine\ORM\EntityManager;
+use AppBundle\Service\SSH\ScheduleSSH;
 
 class ContainerListener
 {
@@ -25,14 +27,16 @@ class ContainerListener
     protected $stateApi;
     protected $operationApi;
     protected $profileManagerApi;
+    protected $sshApi;
 
-    public function __construct(EntityManager $em, ContainerApi $api, ContainerStateApi $stateApi, OperationApi $operationApi, ProfileManagerApi $profileManagerApi)
+    public function __construct(EntityManager $em, ContainerApi $api, ContainerStateApi $stateApi, OperationApi $operationApi, ProfileManagerApi $profileManagerApi, ScheduleSSH $sshApi)
     {
         $this->em = $em;
         $this->api = $api;
         $this->stateApi = $stateApi;
         $this->operationApi = $operationApi;
         $this->profileManagerApi = $profileManagerApi;
+        $this->sshApi = $sshApi;
     }
 
     /**
@@ -41,16 +45,17 @@ class ContainerListener
      * @throws \Doctrine\ORM\OptimisticLockException
      * @throws \Httpful\Exception\ConnectionErrorException
      */
-    public function onLxdContainerCreationUpdate(ContainerCreationEvent $event){
+    public function onLxdContainerCreationUpdate(ContainerCreationEvent $event)
+    {
 
-        echo "START-CREATION : ContainerId ".$event->getContainerId()." \n";
+        echo "START-CREATION : ContainerId " . $event->getContainerId() . " \n";
 
         echo "CREATING CONTAINER... \n";
 
         $operationsResponse = $this->api->getOperationsLinkWithWait($event->getHost(), $event->getOperationId());
 
         if ($operationsResponse->body->metadata->status_code != 200) {
-            echo "FAILED-UPDATE : ".$operationsResponse->body->metadata->err."\n";
+            echo "FAILED-UPDATE : " . $operationsResponse->body->metadata->err . "\n";
             $container = $this->em->getRepository(Container::class)->find($event->getContainerId());
             $container->setState($operationsResponse->body->metadata->err);
             $this->em->persist($container);
@@ -66,7 +71,7 @@ class ContainerListener
         $this->em->persist($container);
         $this->em->flush($container);
 
-        echo "FINISH-CREATION : ContainerId ".$event->getContainerId()."\n";
+        echo "FINISH-CREATION : ContainerId " . $event->getContainerId() . "\n";
     }
 
 
@@ -78,7 +83,7 @@ class ContainerListener
      */
     public function onLxdContainerStateUpdate(ContainerStateEvent $event)
     {
-        echo "START-STATE-UPDATE: ContainerId ".$event->getContainerId()." \n";
+        echo "START-STATE-UPDATE: ContainerId " . $event->getContainerId() . " \n";
 
 
         echo "UPDATING STATE... \n";
@@ -86,7 +91,7 @@ class ContainerListener
         $operationsResponse = $this->stateApi->getOperationsLinkWithWait($event->getHost(), $event->getOperationId());
 
         if ($operationsResponse->body->metadata->status_code != 200) {
-            echo "FAILED-STATE-UPDATE : ".$operationsResponse->body->metadata->err."\n";
+            echo "FAILED-STATE-UPDATE : " . $operationsResponse->body->metadata->err . "\n";
             $container = $this->em->getRepository(Container::class)->find($event->getContainerId());
             $container->setState($operationsResponse->body->metadata->err);
             $this->em->persist($container);
@@ -104,7 +109,7 @@ class ContainerListener
         $this->em->persist($container);
         $this->em->flush($container);
 
-        echo "FINISH-STATE-UPDATE : ContainerId ".$event->getContainerId()."\n";
+        echo "FINISH-STATE-UPDATE : ContainerId " . $event->getContainerId() . "\n";
     }
 
     /**
@@ -115,7 +120,7 @@ class ContainerListener
      */
     public function onLxdContainerDeleteUpdate(ContainerDeleteEvent $event)
     {
-        echo "START-CONTAINER-DELETE: ContainerId ".$event->getContainerId()." \n";
+        echo "START-CONTAINER-DELETE: ContainerId " . $event->getContainerId() . " \n";
 
 
         echo "DELETING Container... \n";
@@ -123,7 +128,7 @@ class ContainerListener
         $operationsResponse = $this->operationApi->getOperationsLinkWithWait($event->getHost(), $event->getOperationId());
 
         if ($operationsResponse->body->metadata->status_code != 200) {
-            echo "FAILED-CONTAINER-DELETE : ".$operationsResponse->body->metadata->err."\n";
+            echo "FAILED-CONTAINER-DELETE : " . $operationsResponse->body->metadata->err . "\n";
             $container = $this->em->getRepository(Container::class)->find($event->getContainerId());
             $container->setState($operationsResponse->body->metadata->err);
             $this->em->flush($container);
@@ -132,14 +137,65 @@ class ContainerListener
 
         $container = $this->em->getRepository(Container::class)->find($event->getContainerId());
 
-        foreach ($container->getProfiles() as $profile){
+        foreach ($container->getBackupSchedules() as $schedule) {
+            echo "REMOVE-CONTAINER-FROM-BACKUPSCHEDULE \n";
+            $sshApi->deleteAnacronFile($schedule);
+
+            $schedule->removeContainer($container);
+
+            $sshApi->sendAnacronFile($schedule);
+            $sshApi->makeFileExecuteable($schedule);
+        }
+
+        foreach ($container->getProfiles() as $profile) {
             $this->profileManagerApi->disableProfileForContainer($profile, $container);
         }
 
         $this->em->remove($container);
         $this->em->flush($container);
 
-        echo "FINISH-CONTAINER-DELETE : ContainerId ".$event->getContainerId()."\n";
+        echo "FINISH-CONTAINER-DELETE : ContainerId " . $event->getContainerId() . "\n";
+    }
+
+
+    /**
+     * @param ContainerUpdateEvent $event
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \Httpful\Exception\ConnectionErrorException
+     */
+    public function onLxdContainerUpdateUpdate(ContainerUpdateEvent $event)
+    {
+        echo "START-CONTAINER-UPDATE: ContainerId " . $event->getContainerId() . " \n";
+
+
+        echo "UPDATING Container... \n";
+
+        $operationsResponse = $this->operationApi->getOperationsLinkWithWait($event->getHost(), $event->getOperationId());
+
+        if ($operationsResponse->body->metadata->status_code != 200) {
+            echo "FAILED-CONTAINER-UPDATE : " . $operationsResponse->body->metadata->err . "\n";
+            $container = $this->em->getRepository(Container::class)->find($event->getContainerId());
+            $container->setState($operationsResponse->body->metadata->err);
+            $this->em->flush($container);
+            return;
+        }
+
+        $container = $this->em->getRepository(Container::class)->find($event->getContainerId());
+
+        echo "UPDATING-BACKUPSCHEDULE... \n";
+        foreach ($container->getBackupSchedules() as $schedule) {
+            $sshApi->deleteAnacronFile($schedule);
+
+            $sshApi->sendAnacronFile($schedule);
+            $sshApi->makeFileExecuteable($schedule);
+        }
+
+        $container->setSettings($operationsResponse->body);
+
+        $this->em->flush($container);
+
+        echo "FINISH-CONTAINER-UPDATE : ContainerId " . $event->getContainerId() . "\n";
     }
 
 }
