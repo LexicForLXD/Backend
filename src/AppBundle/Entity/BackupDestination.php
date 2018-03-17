039c3da5de34832aace36edf3474e0b57e2f45fc
Leon Lenzen <ich@leonlenzen.de>
2018-03-18 00:12:51 +0100

    refactor: make choice strict

--- src/AppBundle/Entity/BackupDestination.php
+++ src/AppBundle/Entity/BackupDestination.php
@@ -57,7 +57,7 @@ class BackupDestination
      * @ORM\Column(type="string")
      * @Assert\NotBlank()
      * @Assert\Type("string")
-     * @Assert\Choice({"scp", "ftp", "file", "imap", "imaps", "rsync", "sftp", "cf+http", "http", "https", "s3", "s3+http", "u1", "u1+http", "tahoe", "webdav", "webdavs", "gdocs"})
+     * @Assert\Choice(choices={"scp", "ftp", "file", "imap", "imaps", "rsync", "sftp", "cf+http", "http", "https", "s3", "s3+http", "u1", "u1+http", "tahoe", "webdav", "webdavs", "gdocs"}, strict="true")
      * @OAS\Property(example="scp")
      * @var string
      */
