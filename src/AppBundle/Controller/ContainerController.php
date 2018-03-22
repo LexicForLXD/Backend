c6ec795cdea42e6167ea3f9fb95eb5db469e6523
Leon Lenzen <ich@leonlenzen.de>
2018-03-22 18:43:48 +0100

    fix: fixes #50 added validation for profiles while container create and update. Will fail with 400 error and list of profiles that are not present in our db.

--- src/AppBundle/Controller/ContainerController.php
+++ src/AppBundle/Controller/ContainerController.php
@@ -32,10 +32,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Swagger\Annotations as OAS;
 
 
-use \Symfony\Component\VarDumper\VarDumper;
-
-
-
 
 class ContainerController extends Controller
 {
@@ -112,6 +108,7 @@ class ContainerController extends Controller
      * @param ContainerApi $api
      * @param EntityManagerInterface $em
      * @return Response
+     * @throws ElementNotFoundException
      * @throws \Httpful\Exception\ConnectionErrorException
      */
     public function listFromHostAction(Request $request, int $hostId, ContainerApi $api, EntityManagerInterface $em)
@@ -361,9 +358,11 @@ class ContainerController extends Controller
      * @param EntityManagerInterface $em
      * @param ContainerApi $api
      * @param ProfileManagerApi $profileManagerApi
+     * @param HostApi $hostApi
+     * @param OperationApi $operationApi
      * @return Response
      * @throws ElementNotFoundException
-     * @throws WrongInputException
+     * @throws WrongInputExceptionArray
      * @throws \Doctrine\ORM\ORMException
      * @throws \Doctrine\ORM\OptimisticLockException
      * @throws \Httpful\Exception\ConnectionErrorException
@@ -382,6 +381,8 @@ class ContainerController extends Controller
         $type = $request->query->get('type');
 
         $profiles = $this->getDoctrine()->getRepository(Profile::class)->findBy(['id' => $request->get("profiles")]);
+
+        $this->checkProfiles($profiles, $request->get("profiles"));
 
         $profileNames = array();
 
@@ -587,6 +588,7 @@ class ContainerController extends Controller
      * @param ContainerApi $api
      * @param EntityManagerInterface $em
      * @return Object|Response
+     * @throws ElementNotFoundException
      * @throws \Httpful\Exception\ConnectionErrorException
      */
     public function showSingleAction(Request $request, int $containerId, ContainerApi $api, EntityManagerInterface $em)
@@ -643,7 +645,11 @@ class ContainerController extends Controller
      * @param int $containerId
      * @param EntityManagerInterface $em
      * @param ContainerApi $api
+     * @param ContainerStateApi $stateApi
      * @return JsonResponse
+     * @throws ElementNotFoundException
+     * @throws WrongInputException
+     * @throws \Httpful\Exception\ConnectionErrorException
      */
     public function deleteAction(int $containerId, EntityManagerInterface $em, ContainerApi $api, ContainerStateApi $stateApi)
     {
@@ -686,7 +692,10 @@ class ContainerController extends Controller
      * @param EntityManagerInterface $em
      * @param ContainerApi $api
      * @param OperationApi $operationApi
+     * @return Response
+     * @throws ElementNotFoundException
      * @throws WrongInputException
+     * @throws WrongInputExceptionArray
      * @throws \Httpful\Exception\ConnectionErrorException
      * @Route("/containers/{containerId}", name="containers_update", methods={"PUT"})
      *
@@ -795,6 +804,8 @@ class ContainerController extends Controller
                 $profileNames[] = $profile->getName();
             }
 
+            $this->checkProfiles($profiles, $request->get("profiles"));
+
             if (!$request->request->has("architecture") && !$request->request->has("config") && !$request->request->has("devices") && !$request->request->has("ephemeral")) {
                 throw new WrongInputException("The following fields are all required: architecture, config, devices, profiles and ephemeral");
             }
@@ -838,6 +849,7 @@ class ContainerController extends Controller
      * Validates a Container Object and returns array with errors.
      * @param Container $object
      * @return array|bool
+     * @throws WrongInputExceptionArray
      */
     private function validation(Container $object)
     {
@@ -850,10 +862,38 @@ class ContainerController extends Controller
                 $errorArray[$error->getPropertyPath()] = $error->getMessage();
             }
             throw new WrongInputExceptionArray($errorArray);
-            return $errorArray;
-
         }
         return false;
+    }
+
+
+    /**
+     * @param array $profiles
+     * @param array $profilesRequest
+     * @return bool
+     * @throws WrongInputExceptionArray
+     */
+    private function checkProfiles(Array $profiles, Array $profilesRequest)
+    {
+        if(count($profiles) == count($profilesRequest))
+        {
+            return true;
+        }
+
+        $profilesDB = array();
+
+        foreach ($profiles as $profile)
+        {
+            $profilesDB[] = $profile->getId();
+        }
+
+        $errors = array_diff($profilesRequest, $profilesDB);
+
+        $errorArray = array();
+        foreach ($errors as $error) {
+            $errorArray[] = 'The profile with the id ' . $error . ' is not present in our database.';
+        }
+        throw new WrongInputExceptionArray($errorArray);
     }
 
 }
