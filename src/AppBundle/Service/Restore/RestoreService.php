d7acc31d3936560eff74f2e9cc75632bb2c936bf
Jan Ritter <git@janrtr.de>
2018-04-01 19:11:01 +0200

    refactor: get tarball list from specified host, instead of localhost

--- src/AppBundle/Service/Restore/RestoreService.php
+++ src/AppBundle/Service/Restore/RestoreService.php
@@ -36,23 +36,32 @@ class RestoreService
 
     /**
      * @param Backup $backup
+     * @param Host $host
      * @return string | array
      */
-    public function getFilesInBackupForTimestamp(Backup $backup)
+    public function getFilesInBackupForTimestamp(Backup $backup, Host $host)
     {
         $backupDestination = $backup->getDestination();
         $backupSchedule = $backup->getBackupSchedule();
 
         $remoteBackupPath = $backupDestination->getDestinationText().$backupSchedule->getName();
 
-        exec('duplicity list-current-files --time '.date_format($backup->getTimestamp(), DATE_ATOM).' '.$remoteBackupPath.' 2>&1 &', $result);
+        $hostname = $host->getIpv4() ? : $host->getIpv6() ? : $host->getDomainName() ? : 'localhost';
+        $configuration = new Configuration($hostname);
+        $authentication = new PublicKeyFile($this->ssh_user, $this->ssh_location, $this->ssh_key_location, $this->ssh_passphrase);
+
+        $session = new Session($configuration, $authentication);
+
+        $exec = $session->getExec();
+
+        $result = $exec->run('duplicity list-current-files --time '.date_format($backup->getTimestamp(), DATE_ATOM).' '.$remoteBackupPath.' 2>&1 &');
 
         if(strpos($result, 'Error') !== false){
             return substr($result, strpos($result, 'Error'));
         }
 
         //Second query with grep
-        exec('duplicity list-current-files --time '.date_format($backup->getTimestamp(), DATE_ATOM).' '.$remoteBackupPath.' | grep .tar.gz', $result);
+        $result = $exec->run('duplicity list-current-files --time '.date_format($backup->getTimestamp(), DATE_ATOM).' '.$remoteBackupPath.' | grep .tar.gz');
 
         //Create array with filenames from result - try to find all files ending with .tar.gz
         $numberOfTarballs = substr_count($result, '.tar.gz');
