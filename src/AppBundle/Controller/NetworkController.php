0fe7fa1b4caec9800a3f5a18f2a625d3baa18fe8
Jan Ritter <git@janrtr.de>
2018-04-15 12:26:28 +0200

    feat: added getSingleNetwork endpoint

--- src/AppBundle/Controller/NetworkController.php
+++ src/AppBundle/Controller/NetworkController.php
@@ -28,8 +28,24 @@ class NetworkController extends Controller
         return new Response($response);
     }
 
-    public function getSingleNetwork(){
+    /**
+     * @Route("/networks/{networkID}", name="get_single_networks", methods={"GET"})
+     * @param $networkID
+     * @return Response
+     * @throws ElementNotFoundException
+     */
+    public function getSingleNetwork($networkID){
+        $network = $this->getDoctrine()->getRepository(Network::class)->find($networkID);
 
+        if(!$network){
+            throw new ElementNotFoundException(
+                'No Network for '.$networkID.' found'
+            );
+        }
+
+        $serializer = $this->get('jms_serializer');
+        $response = $serializer->serialize($network, 'json');
+        return new Response($response);
     }
 
     public function createNetwork(){
