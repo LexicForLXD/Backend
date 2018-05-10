f6cc9b4c9d0b6d5760d783dfa7f3e2c2401001c0
Leon Lenzen <ich@leonlenzen.de>
2018-05-10 22:51:20 +0200

    feat: image worker

---
+++ src/AppBundle/Worker/ImageWorker.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: leon
+ * Date: 10.05.18
+ * Time: 22:23
+ */
+
+namespace AppBundle\Worker;
+
+
+use AppBundle\Entity\Image;
+use AppBundle\Service\LxdApi\ImageApi;
+use AppBundle\Service\LxdApi\OperationApi;
+use Doctrine\ORM\EntityManagerInterface;
+use Dtc\QueueBundle\Model\Worker;
+
+class ImageWorker extends Worker
+{
+    protected $em;
+    protected $api;
+    protected $operationApi;
+
+    /**
+     * ImageWorker constructor.
+     * @param EntityManagerInterface $em
+     * @param ImageApi $api
+     * @param OperationApi $operationApi
+     */
+    public function __construct(EntityManagerInterface $em, ImageApi $api, OperationApi $operationApi)
+    {
+        $this->em = $em;
+        $this->api = $api;
+        $this->operationApi = $operationApi;
+    }
+
+    /**
+     * @param Image $image
+     * @param $body
+     * @throws \Httpful\Exception\ConnectionErrorException
+     */
+    public function createImage(Image $image, $body)
+    {
+        $result = $this->api->createImage($image->getHost(), $body);
+
+        if ($result->code != 202) {
+            $image->setError($result->body->error);
+        }
+        if ($result->body->metadata->status_code == 400) {
+            $image->setError($result->body->error);
+        }
+
+        $operationsResponse = $this->operationApi->getOperationsLinkWithWait($image->getHost(), $result->body->metadata->id);
+
+        if ($operationsResponse->body->metadata->status_code != 200) {
+            $image->setError($operationsResponse->body->metadata->err);
+            $this->em->persist($image);
+            $this->em->flush($image);
+            return;
+        }
+
+        $image->setFingerprint($operationsResponse->body->metadata->metadata->fingerprint);
+
+        //Parse architecture
+        $result = $this->api->getImageByFingerprint($image->getHost(), $image->getFingerprint());
+        $image->setArchitecture($result->body->metadata->architecture);
+        $image->setSize($operationsResponse->body->metadata->metadata->size);
+        $image->setFinished(true);
+
+        $this->em->persist($image);
+        $this->em->flush($image);
+    }
+
+    public function getName()
+    {
+        return "image";
+    }
+}
