a40cd2f8d7616133a9ecd4718382e17a109fb2b8
Leon Lenzen <ich@leonlenzen.de>
2018-05-28 03:44:38 +0200

    feat: proxy login first try

---
+++ src/AppBundle/Controller/LoginController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: leon
+ * Date: 28.05.18
+ * Time: 02:36
+ */
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller as BaseController;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+
+
+class LoginController extends BaseController
+{
+
+    /**
+     * @Route("/login", name="login_proxy", methods={"POST"})
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function loginAction(Request $request)
+    {
+        $oauthRequest = Request::create('/oauth/v2/token', 'POST', [
+            "grant_type" => "password",
+            "client_id" => $this->getParameter('client_id'),
+            "client_secret" => $this->getParameter('client_secret'),
+            "username" => $request->get("username"),
+            "password" => $request->get("password")
+        ], $request->cookies->all(), $request->files->all(), $request->server->all(), $request->getContent());
+
+        return $this->forward('FOSOAuthServerBundle:Token:token', ["request" => $oauthRequest]);
+
+    }
+}
