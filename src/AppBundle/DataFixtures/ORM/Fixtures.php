a1dfc7d91dc231899081b075185ae48f6a12e111
Leon Lenzen <ich@leonlenzen.de>
2018-05-29 18:21:21 +0200

    test: proxy tests

--- src/AppBundle/DataFixtures/ORM/Fixtures.php
+++ src/AppBundle/DataFixtures/ORM/Fixtures.php
@@ -26,7 +26,7 @@ class Fixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $client = new Client();
-        $client->setAllowedGrantTypes(['password']);
+        $client->setAllowedGrantTypes(['password', 'refresh_token']);
         $client->setSecret('4ok2x70rlfokc8g0wws8c8kwcokw80k44sg48goc0ok4w0so0k');
         $client->setRandomId('3bcbxd9e24g0gk4swg0kwgcwg4o8k8g4g888kwc44gcc0gwwk4');
         $manager->persist($client);
